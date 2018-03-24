e7e75d4a79dc56f44d6741a1db143715784397d6
aurelien <al@webserv>
2018-03-24 18:38:17 +0100

    Typo in Book Instance controler

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -5,7 +5,7 @@ exports.bookinstance_list = function(req, res, next) {
 
     BookInstance.find()
         .populate('book')
-        .exex(function (err, list_bookinstances) {
+        .exec(function (err, list_bookinstances) {
             if (err) {return next(err);}
             //Successful, so render
             res.render('bookinstance_list', { title: 'Book Instance List', bookinstance_list: list_bookinstances});
