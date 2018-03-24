c0170e4ced57b6762da190597ef5f912c5b0b20e
aurelien <al@webserv>
2018-03-24 22:29:12 +0100

    creation of the pages for the book, author, book instances and genre details

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -7,8 +7,8 @@ var BookInstance = require('../models/bookinstance');
 
 var async = require('async');
 
-exports.index = function(req, res) {   
-    
+exports.index = function(req, res) {
+
     async.parallel({
         book_count: function(callback) {
             Book.count(callback);
@@ -42,8 +42,32 @@ exports.book_list = function(req, res,next) {
 };
 
 // Display detail page for a specific book.
-exports.book_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book detail: ' + req.params.id);
+exports.book_detail = function(req, res, next) {
+
+    async.parallel({
+        book: function(callback) {
+            Book.findById(req.params.id)
+                .populate('author')
+                .populate('genre')
+                .exec(callback);
+        },
+
+        book_instances: function(callback) {
+            BookInstance.find({ 'book': req.params.id})
+            .exec(callback);
+        },
+
+    }, function(err, results) {
+        if (err) {return next(err); }
+        if (results.book==null) { //No results.
+            var err = new Error('Book not found');
+            err.status = 404;
+            return next(err);
+        }
+        //Successful, so render
+        res.render('book_detail', {title: 'Title', book: results.book, book_instances: results.book_instances });
+    });
+    //    res.send('NOT IMPLEMENTED: Book detail: ' + req.params.id);
 };
 
 // Display book create form on GET.
